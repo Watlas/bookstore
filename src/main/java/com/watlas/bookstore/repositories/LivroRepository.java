07de0c1cc348260ea9f826dff3fc3e19f176c4e4
watlas <watlasrick@hotmail.com>
2021-02-12 16:15:16 -0300

    repository

---
+++ src/main/java/com/watlas/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,11 @@
+package com.watlas.bookstore.repositories;
+
+import com.watlas.bookstore.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Categoria, Integer> {
+
+
+}
